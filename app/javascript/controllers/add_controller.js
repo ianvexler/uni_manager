ccd7f869d8e7da145c477bfef4b0b80a4799da4c
Ian Vexler <ianvexler@gmail.com>
2022-06-20 18:49:28 +0100

    Basic style design finished.

--- app/javascript/controllers/add_controller.js
+++ app/javascript/controllers/add_controller.js
@@ -4,11 +4,12 @@ export default class extends Controller {
     open(event) {
         document.getElementById("popup_form").style.display = "block";
         document.getElementById("blur_background").style.display = "block";
+        document.body.style.overflow = "hidden";
     }
 
     close(event) {
         document.getElementById("popup_form").style.display = "none";
         document.getElementById("blur_background").style.display = "none";
-        
+        document.body.style.overflow = "visible";
     }
 }
